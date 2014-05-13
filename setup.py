58cdba0a3e4c8c90cc106c43ae5af0f50d0402ef
krishnat01 <krt3@rice.edu>
2014-05-12 21:13:45 -0500

    Tests for ORM

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from app import db
+from app.models import Course, Exam
+import datetime
 
 def setup_all():
     setup_db()
+    setup_fake()
 
 def setup_db():
     db.drop_all()
     db.create_all()
 
+def setup_fake():
+    fake_course1 = Course(department_name = "COMP", number = "182")
+    fake_course2 = Course(department_name = "ECON", number = "201")
+
+    fake_exam1 = Exam(start_time = datetime.datetime.today(), end_time = datetime.datetime.today(), course = fake_course1, section = "001", teacher = "Luay Nakleh", location = "KCK 100")
+    fake_exam2 = Exam(start_time = datetime.datetime.today(), end_time = datetime.datetime.today(), course = fake_course2, section = "003", teacher = "John Diamond", location = "HRZ AMP")
+    db.session.add_all([fake_course1,fake_course2,fake_exam1,fake_exam2])
+    db.session.commit()
+
 if __name__ == "__main__":
     setup_all()
+
