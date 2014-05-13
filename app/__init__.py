d1e1cfc9d5ad279a168277291b525a8e2ae8173c
Ethan Steinberg <ethan.steinberg@gmail.com>
2014-05-12 20:38:38 -0500

    Models added

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///foo.db"
+
+db = SQLAlchemy(app)
 
 import views
+import models
