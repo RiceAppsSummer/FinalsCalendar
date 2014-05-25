231abbe79e3a35a3b81501979959c089ec335878
Yoko Hongyu Li <yokolee1013@gmail.com>
2014-05-25 13:49:58 -0500

    Courses endpoint implimented

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
-from flask import render_template
+from flask import render_template,jsonify
 from app import app,db
 import datetime
+import json
 import download
 from app.models import Course,Exam
-
+from collections import defaultdict
 
 @app.route('/')
 def test():
@@ -11,15 +12,22 @@ def test():
 
 @app.route("/courses")
 def courses():
-	mapping = {"courseName":{"teacherName":{"section":[datetime.datetime.today()]}}}
-	courses = db.session.query(Course,Exam.teacher).join(Course.exams).group_by(Exam.teacher)
-	print list(courses)
-	for course in courses:
-		course_name = str(course[0])
-		mapping[course_name] = {str(course[1])}
+	def create_inner_default_dict():
+		return defaultdict(dict)
 
-		# print course.exams.group_by(Exam.teacher)
-		
+	mapping = defaultdict(create_inner_default_dict)
+	courses_exams = db.session.query(Course,Exam).join(Course.exams)
+
+	for course_exam in courses_exams:
+		course_name = str(course_exam[0])
+		teacher = course_exam[1].teacher
+		section = course_exam[1].section
+		s_time = course_exam[1].start_time.isoformat()
+		e_time = course_exam[1].end_time.isoformat()
+		mapping[course_name][teacher][section] = {"start_time":s_time,"end_time":e_time}
+
+	return jsonify(**mapping)
+
 	
 
 
