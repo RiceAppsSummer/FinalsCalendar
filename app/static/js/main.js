0d46df2f8c267e0ea2bb97261e0a27dbca576388
krishnat01 <krt3@rice.edu>
2014-05-26 10:58:03 -0500

    Implemented Adding and removing events from calendar

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,14 +1,50 @@
 $(document).ready(function(){
-
+	var currentCourses = [];
+	function disableTeachAndSect(){
+		$("#teacher").prop('disabled', true);
+		$("#teacher").empty();
+		var noTeach = $('<option></option>').text("Please type in a Course");
+		$("#teacher").append(noTeach);
+		$("#section").prop('disabled', true);
+		$("#section").empty();
+		var noSect = $('<option></option>').text("Please select a teacher");
+		$("#section").append(noSect);
+	}
 	$('#calendar').fullCalendar({
         // put your options and callbacks here
-          // defaultView: "agendaWeek"
+        defaultView: "agendaWeek",
+        defaultDate: "2014-04-30", 
+        timezone:"UTC"
         });
 	$("#coursenumber").prop('disabled', true);
-	$("#teacher").prop('disabled', true);
-	$("#section").prop('disabled', true);
+	disableTeachAndSect();
 	$.getJSON("/courses",function(courses){
+		$("#add").click(function(){
+			var course = $("#coursenumber").val();
+			if(courses.hasOwnProperty(course)&& currentCourses.indexOf(course)==-1){
+				var teacher = $("#teacher").val();
+				var section = $("#section").val();
+				var stime = courses[course][teacher][section].start_time
+				var etime = courses[course][teacher][section].end_time
+				console.log(courses[course][teacher][section].start_time);
+				console.log(stime);
+				var examDuration = {title:course,start:stime,end:etime,allDay:false,id:course}
+				$("#calendar").fullCalendar('addEventSource',[examDuration])
+				currentCourses.push(course)
+				var deleteButton = $('<a class="btn btn-danger" href="#"> <i class="glyphicon glyphicon-trash"></i></a>');
+				var courseText = $('<span></span>').text(course).addClass("addedCourse")
+				var courseAdded = $('<li></li>').addClass("list-group-item").append(courseText).append(deleteButton);
+				$("#currentCourses").append(courseAdded);
+				deleteButton.click(function(){
+					courseAdded.remove();
+					index = currentCourses.indexOf(course);
+					currentCourses.splice(index,1);
+					$("#calendar").fullCalendar('removeEvents',[course])
 
+
+				})
+			}
+		})
 		console.log(courses);
 		$("#coursenumber").prop('disabled', false);
 	    $("#coursenumber").autocomplete({
@@ -39,7 +75,11 @@ $(document).ready(function(){
 	    		 
 	    		$("#teacher").change(); 
 
-	    	} 
+	    	}
+	    	else{
+	    		disableTeachAndSect();
+	    	}
+
 	    	
 	    }); // coursenumer.keyup()
 	});
